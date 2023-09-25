b6abf6443312c66479a76a8959da5e88b0db7fdc
mariluciasoares89 <mari@mari>
2023-09-25 16:13:44 -0300

    Criando SQL e método que consulta o usuário por login

--- curso-jsp-jsf/src/main/java/dao/DAOUsuarioRepository.java
+++ curso-jsp-jsf/src/main/java/dao/DAOUsuarioRepository.java
@@ -2,31 +2,62 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import connection.SingleConnectionBanco;
 import model.ModelLogin;
 
 public class DAOUsuarioRepository {
-
+	
+	
 	private Connection connection;
+	
 	
 	public DAOUsuarioRepository() {
 		connection = SingleConnectionBanco.getConnection();
+	}
+	
+	public ModelLogin gravarUsuario(ModelLogin objeto) throws Exception {
+		
+		String sql = "INSERT INTO model_login(login, senha, nome, email)  VALUES (?, ?, ?, ?);";
+		PreparedStatement preparedSql = connection.prepareStatement(sql);
+		
+		preparedSql.setString(1, objeto.getLogin());
+		preparedSql.setString(2, objeto.getSenha());
+		preparedSql.setString(3, objeto.getNome());
+		preparedSql.setString(4, objeto.getEmail());
+		
+		preparedSql.execute();
+		
+		connection.commit();
 		
 		
+		return this.consultaUsuario(objeto.getLogin());
 	}
-	public void gravarUsuario(ModelLogin Objeto)throws Exception {
-		String sql = "INSERT INTO model_login (login, senha, nome, email) VAlUES (?, ?, ?, ?);";
-			PreparedStatement preparedSql = connection.prepareStatement(sql);
+	
+	
+	public ModelLogin consultaUsuario(String login) throws Exception  {
+		
+		ModelLogin modelLogin = new ModelLogin();
+		
+		String sql = "select * from model_login where upper(login) = upper('"+login+"')";
+		
+		PreparedStatement statement = connection.prepareStatement(sql);
+		
+		ResultSet resutlado =  statement.executeQuery();
+		
+		while (resutlado.next()) /*Se tem resultado*/ {
 			
-			preparedSql.setString(1, Objeto.getLogin());
-			preparedSql.setString(2, Objeto.getSenha());
-			preparedSql.setString(3, Objeto.getNome());
-			preparedSql.setString(4, Objeto.getEmail());
-			
-			
-			preparedSql.execute();
-			connection.commit();
-			
+			modelLogin.setId(resutlado.getLong("id"));
+			modelLogin.setEmail(resutlado.getString("email"));
+			modelLogin.setLogin(resutlado.getString("login"));
+			modelLogin.setSenha(resutlado.getString("senha"));
+			modelLogin.setNome(resutlado.getString("nome"));
+		}
+		
+		
+		return modelLogin;
+		
 	}
+
 }
--- curso-jsp-jsf/src/main/java/servlets/ServletUsuarioController.java
+++ curso-jsp-jsf/src/main/java/servlets/ServletUsuarioController.java
@@ -15,25 +15,23 @@ import model.ModelLogin;
 
 @WebServlet("/ServletUsuarioController")
 public class ServletUsuarioController extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
-     
-	private   DAOUsuarioRepository daoUsuarioRepository = new DAOUsuarioRepository();
-  
+	
+	
+	private DAOUsuarioRepository daoUsuarioRepository = new DAOUsuarioRepository();
+
     public ServletUsuarioController() {
-        
-    
     }
 
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
 
+	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		
 		try {
+		
 		String id = request.getParameter("id");
 		String nome = request.getParameter("nome");
 		String email = request.getParameter("email");
@@ -49,21 +47,20 @@ public class ServletUsuarioController extends HttpServlet {
 		modelLogin.setSenha(senha);
 		
 		
-		daoUsuarioRepository.gravarUsuario(modelLogin);
-		
-		request.setAttribute("msg", "Operação realizada com sucesso!;");
-		request.setAttribute("modelLogin", modelLogin);
-	    request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);;
+		modelLogin = daoUsuarioRepository.gravarUsuario(modelLogin);
 		
 		
+		request.setAttribute("msg", "Operação realizada com sucesso!");
+		request.setAttribute("modolLogin", modelLogin);
+		request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
 		
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-			RequestDispatcher redirecionar = request.getRequestDispatcher("/erro.jsp");
+			RequestDispatcher redirecionar = request.getRequestDispatcher("erro.jsp");
 			request.setAttribute("msg", e.getMessage());
 			redirecionar.forward(request, response);
-
 		}
+		
 	}
 
 }
